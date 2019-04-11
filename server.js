08b2c5e569654398f00b5e3a15e0ade27ab50a49
Storozhuk Nikita <storozhuk.nikita@gmail.com>
2019-04-11 21:53:51 +0300

    Add changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express'); // фреймворк
 const fs = require('fs'); // файловая система
 const bodyParser = require('body-parser');
+const moment = require('moment');
 
 
 const app = express();
@@ -53,27 +54,97 @@ app.post('/cart', (req, res) => {
 // запрос на изменение
 app.patch('/cart/:id', (req, res) => {
     fs.readFile('./db/cart.json', 'utf-8', (err, data) => {
-        if(err){
+        if(err) {
             return console.log(err);
         }
 
         let cart = JSON.parse(data);
+
         cart = cart.map((item) => {
-            if(item.id === +req.params.id){
-                return {...item, ...req.body};
+            if(item.id === +req.params.id) {
+                return { ...item, ...req.body };
             }
+
             return item;
         });
 
         fs.writeFile('./db/cart.json', JSON.stringify(cart), (err) => {
-            if(err){
-                console.log(err);
+            if(err) {
+                return console.log(err);
             }
+
             res.send(cart.find((item) => item.id === +req.params.id));
         });
-
-    })
+    });
 });
+
+
+
+app.delete('/cart/:id', (req, res) => {
+   fs.readFile('./db/cart.json', 'utf-8', (err, data) => {
+       if(err){
+           return console.log(err);
+       }
+
+       let cart = JSON.parse(data);
+       const deleted = cart.find((item) => item.id === +req.params.id);
+       cart = cart.filter((item) => item.id !== +req.params.id);
+
+       fs.writeFile('./db/cart.json', JSON.stringify(cart), (err) => {
+           if(err){
+               return console.log(err);
+           }
+
+           res.send(deleted);
+
+       });
+
+   })
+});
+
+app.use('/cart', (req, res, next) => {
+    if((['POST', 'PATCH', 'DELETE'].includes(req.method))){
+        const mapping = {
+            'POST': 'Добавление',
+            'PATCH': 'Редактирование',
+            'DELETE': 'Удаление',
+        };
+
+        fs.readFile('./db/stats.json', 'utf-8', (err, data) => {
+            const stats = JSON.parse(data);
+
+            switch(req.method){
+                case 'POST':
+                    stats.push({
+                        action: mapping[req.method],
+                        name: req.body.name,
+                        timestamp: moment().format(),
+                    });
+                    fs.writeFile('./db/stats.json', JSON.stringify(stats));
+                break;
+
+                case 'PATH':
+                case 'DELETE':
+                    const [, id] = req.url.split('/'); // получаем id
+                    fs.readFile('./db/products.json', 'utf-8', (err, data) => {
+                       const products = JSON.parse(data);
+                       const product = products.find((item) => item.id === +id);
+                        stats.push({
+                            action: mapping[req.method],
+                            name: req.body.name,
+                            timestamp: moment().format(),
+                        });
+                        fs.writeFile('./db/stats.json', JSON.stringify(stats));
+                    });
+                break;
+            }
+        });
+
+        console.log(req.method, req.url, req.body);
+    }
+    next();
+});
+
 
 app.listen(3000, () => {
    console.log('server has been starded');
